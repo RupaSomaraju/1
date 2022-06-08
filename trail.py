b05cc3e206641a4659710606c6b2f0b64f2644f5
RupaSomaraju <83549645+RupaSomaraju@users.noreply.github.com>
2022-06-08 08:45:24 +0530

    Add files via upload

---
+++ trail.py
@@ -0,0 +1,27 @@
+import turtle as t
+
+t.pensize(2)
+
+def curve():
+	for i in range(200):
+		t.right(1)
+		t.forward(1)
+
+def heart():
+	t.fillcolor('red')
+	t.begin_fill()
+	t.left(140)
+	t.forward(115)
+	curve()
+	t.left(120)
+	curve()
+	t.forward(112)
+	t.end_fill()
+
+heart()
+
+
+
+
+t.ht()
+t.done()
